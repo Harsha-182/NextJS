4ae99f3b289fb2ffb31d16d2a1e64247e72e9ab8
Harsha <97773381+Harsha-182@users.noreply.github.com>
2025-06-06 18:40:08 +0530

    Routing Metadata

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+	title: "About CodeEvolution"
+}
+
 const about = () => {
 	return <h1>About page</h1>
 }
--- my-app/src/app/products/[productId]/page.tsx
+++ my-app/src/app/products/[productId]/page.tsx
@@ -1,3 +1,21 @@
+import {Metadata} from 'next';
+
+type Props = {
+	params: Promise<{productId: string}>
+}
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> =>{
+	const id = (await params).productId;
+	const title = await new Promise(resolve => {
+		setTimeout(() => {
+			resolve(`iPhone ${id}`);
+		}, 100);
+	})
+	return {
+		title: `Product ${title}`
+	}
+};
+
 const  ProductDetails = async(
 		{params}: 
 		{params: Promise<{ productId: string }>
