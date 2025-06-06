dd1c78565b7abd78f13b61ccae07f474fc39f2d3
Harsha <97773381+Harsha-182@users.noreply.github.com>
2025-06-06 19:12:32 +0530

    Link Component

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link"
+
 const ProductList = () => {
+	let productId = 100;
 	return(
 		<div>
+			<Link href='/home'>Home</Link>
 			<h1>Product List</h1>
-			<h2>Product 2</h2>
-			<h2>Product 3</h2>
-			<h3>Product 4</h3>
+			<h2><Link href='/products/2'>Product 2</Link></h2>
+			<h2><Link href='/products/3' replace>Product 3</Link></h2>
+			<h2><Link href={`/products/${100}`}>Product {productId}</Link></h2>
 		</div>
 	)
 }
