bfded55353b58fbe1cf6925e95b498f75d524280
Harsha <97773381+Harsha-182@users.noreply.github.com>
2025-06-09 12:49:05 +0530

    Error Handling

--- my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,18 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomInt(count: number) {
+	return Math.floor(Math.random() * count);
+}
+
 const ProductReview = async({
 	params
 }: {
 	params: Promise<{productId: string; reviewId: string}>
 }) => {
+	const random = getRandomInt(2);
+	if(random === 1)
+		throw new Error("Error loading review");
+
 	const {reviewId, productId} = await params;
 
 	if(parseInt(reviewId) > 1000) {
