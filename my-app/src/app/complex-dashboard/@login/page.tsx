27b4a92feeaba6df86ce8da1c0726efde157e837
Harsha <97773381+Harsha-182@users.noreply.github.com>
2025-06-11 11:27:51 +0530

    Unmatched Routes & Conditional Routes

---
+++ my-app/src/app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@/components/card"
+import Link from 'next/link';
+
+export default function Login() {
+	return (
+		<Card>
+			<h1>Please login to continue</h1>
+		</Card>
+	)
+}
--- my-app/src/app/complex-dashboard/layout.tsx
+++ my-app/src/app/complex-dashboard/layout.tsx
@@ -1,30 +1,31 @@
-import UserAnalytics from "./@users/page";
-import RevenueMetrics from './@revenue/page';
-import Notifications from './@notifications/page';
-
 export default function ComplexDashboardLayout ({
 	children,
 	users,
 	revenue,
-	notifications
+	notifications,
+	login,
 }:{
 	children: React.ReactNode;
 	users: React.ReactNode;
 	revenue: React.ReactNode;
-	notifications: React.ReactNode
+	notifications: React.ReactNode;
+	login: React.ReactNode;
 }) {
-	return (
+	const isLoggedIn = true;
+	return isLoggedIn? (
 		<>
 			<div>{children}</div> {/* Content from page.tsx*/}
 			<div style={{display: 'flex'}}>
 				<div style={{display: 'flex', flexDirection: 'column'}}>
-					<UserAnalytics/>
-					<RevenueMetrics/>
+					{users}
+					{revenue}
 				</div>
 				<div style={{display: 'flex', flex: 1}}>
-					<Notifications/>
+					{notifications}
 				</div>
 			</div>
 		</>
+		): (
+			login
 	)
 }
